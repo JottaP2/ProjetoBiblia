04e89bac56f8d509960ae87ef8094aa39457d37e
JottaP2 <joao.marinho@arapiraca.ufal.br>
2026-01-09 11:14:37 -0300

    feat: atualização da api

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 const URL = "/api/liturgia";
-//const URL = "http://liturgia.up.railway.app/v2/"
 console.log(URL);
 
-async function liturgiaDiariaApi() {
+async function getLiturgia() {
   try {
     const resposta = await fetch(URL);
     if(resposta.status === 200){
-
       const obj = await resposta.json();
-      document.getElementById('referencia').textContent = obj.leituras.evangelho[0].referencia;
-      document.getElementById('title').textContent = obj.leituras.evangelho[0].titulo;
-      
-      let textoEvangelho = obj.leituras.evangelho[0].texto;
-      textoEvangelho = textoEvangelho.replace(/(\d+)/g, '<span class="numero">$1 </span>');
-      
-      document.getElementById('evangelho').innerHTML = textoEvangelho;
-      
+        return obj
     } else {
       console.error('Erro na requisição:', resposta.status);
     }
@@ -24,4 +15,39 @@ async function liturgiaDiariaApi() {
   }
 }
 
-liturgiaDiariaApi();
+async function loadData() {
+  const getInfo = await getLiturgia()
+  console.log(getInfo)
+
+  const referencia = document.getElementById('referencia')
+  const title = document.getElementById('title')
+  const contentText = document.getElementById('contentText')
+
+  const btnPrimeiraLeitura = document.getElementById('btnPrimeiraLeitura')
+  const btnSalmo = document.getElementById('btnSalmo')
+  const btnEvangelho = document.getElementById('btnEvangelho')
+  
+
+  btnSalmo.addEventListener('click', () => {
+    referencia.innerHTML = getInfo.leituras.salmo[0].referencia
+    title.innerHTML = getInfo.leituras.salmo[0].refrao
+    contentText.innerHTML = getInfo.leituras.salmo[0].texto
+  })
+
+  btnPrimeiraLeitura.addEventListener('click', () => {
+    referencia.innerHTML = getInfo.leituras.primeiraLeitura[0].referencia
+    title.innerHTML = getInfo.leituras.primeiraLeitura[0].titulo
+    contentText.innerHTML = getInfo.leituras.primeiraLeitura[0].texto
+    primeiraLeitura.replace(/(\d+)/g, '<span class="numero">$1 </span>')
+  })
+
+  btnEvangelho.addEventListener('click', () => {
+    referencia.innerHTML = getInfo.leituras.evangelho[0].referencia
+    title.innerHTML = getInfo.leituras.evangelho[0].titulo
+    contentText.innerHTML = getInfo.leituras.evangelho[0].texto
+  })
+
+  btnPrimeiraLeitura.click()
+}
+
+loadData()
